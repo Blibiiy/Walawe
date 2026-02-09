14840ef86f17080dd9e007ace164fb61972fd37a
Isra23343045 <alfattah.isra@gmail.com>
2026-02-09 19:44:01 +0700

    add enemy attack logic. next : fix health bar bug

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -4,15 +4,50 @@ public class EnemyAI : MonoBehaviour
 {
     private NavMeshAgent agent;
     private Transform playerPosition;
+
+    [Header("Enemy Attack Variables")]
+    public float attackRange;
+    public float damage;
+    public float timeBetweenAttacks;
+
+    private float attackTimer = 0f;
+    [SerializeField] private IDamageable playerHealth;
+
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         playerPosition = GameObject.FindGameObjectWithTag("Player").transform;
+        playerHealth = playerPosition.GetComponent<IDamageable>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        agent.SetDestination(playerPosition.position);
+        float distanceToPlayer = Vector3.Distance(transform.position, playerPosition.position);
+        if (distanceToPlayer <= attackRange)
+        {
+            agent.isStopped = true;
+
+            attackTimer -= Time.deltaTime;
+
+            if(attackTimer < 0f)
+            {
+                AttackPlayer();
+                attackTimer = timeBetweenAttacks;
+            }
+        }
+        else
+        {
+            agent.isStopped = false;
+            agent.SetDestination(playerPosition.position);
+        }
+    }
+
+    void AttackPlayer()
+    {
+        if(playerHealth != null)
+        {
+            playerHealth.TakeDamage(damage);
+        }
     }
 }
